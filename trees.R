ab501cf0adf25c46876f4865a97a2ccbe1d3f11f
Arun Ramamurthy <14209371+arun-ramamurthy@users.noreply.github.com>
2020-11-18 19:11:16 -0800

    init

---
+++ trees.R
@@ -0,0 +1,13 @@
+## This file contains utility functions used by the Shiny app.
+
+simulate_buildout <- function(
+  projection_horizon_year = 24,
+  cost_by_generation_in_billions = c(0 = 20, 1 = 20, 2 = 16, 12),
+  new_project_delay_in_years = 4,
+  max_reactor_projects_per_year = 8,
+  num_trees = 3,
+  stagger_time_in_months = 12,
+  start_dates = NULL,
+) {
+
+}
